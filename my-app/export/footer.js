b92320454b7589be211f038bf31513ddbcc9b176
Ayubeey <aubsattorov@gmail.com>
2022-05-06 10:22:49 +0500

    work complet

--- my-app/export/footer.js
+++ my-app/export/footer.js
@@ -4,7 +4,7 @@ import { Text, View, SafeAreaView,Image,StyleSheet,TextInput,Button,Linking } fr
 
 export default function Footer (){
     return(
-        <SafeAreaView style={{height:'100%',backgroundColor:'#575445'}}>
+        <SafeAreaView style={{height:600,backgroundColor:'#575445'}}>
             <View>
                 <Image source={require('../img/dutdoor.svg')} style={{width:200,height:60,marginLeft:'auto',marginRight:'auto',marginTop:20}}/>
             </View>
@@ -36,19 +36,27 @@ export default function Footer (){
                     <Text style={style.subText}>Password</Text>
                 </View>
             </View>
-        <View>
-            <Text>
+        <View style=
+        {{marginTop:25}}>
+            <Text style={{color:'#E6B939',fontSize:20,textAlign:'center',fontFamily:'sans-serif'}}>
             Newsletter
             </Text>
-            <Text>
+            <Text style={{color:'#fff',width:'90%',marginLeft:'auto',marginRight:'auto'}}>
             Subscribe to our (infrequent) newsletter where we share news about upcoming listings and projects.
             </Text>
-            <View>
+            <View style={{marginTop:20}}>
             <TextInput style={style.input} placeholder='Email'/>
-            <a style={{backgroundColor:'#575445',textDecoration:'none',color:'#fff',borderRadius:15,position:'absolute',left:'80%',top:'20%',width:73,height:39,textAlign:'center'}} href='#'>Submit</a>
+            <a style={{backgroundColor:'#575445',textDecoration:'none',color:'#fff',borderRadius:15,position:'absolute',left:'80%',top:'15%',width:73,height:39,textAlign:'center'}} href='#'>Submit</a>
             </View>
             
-            
+            <View><Text style={{width:'90%',marginTop:10,color:'#fff',marginLeft:'auto',marginRight:'auto'}}>© Outdoor Exploration, LLC. All rights reserved. Powered by Webflow</Text>
+            <View style={{flexDirection:'row',justifyContent:'space-around',marginTop:20}}>
+           <a href='#' style={{}}> <Image style={style.subImg} source={require('../img/IN.svg')}/></a>
+           <a href='https://facebook.com/' style={{}}> <Image style={style.subImg} source={require('../img/FACEBOOK.svg')}/></a>
+           <a href='https://www.instagram.com/' style={{}}> <Image style={style.subImg} source={require('../img/insta.svg')}/></a>
+           <a href='https://twitter.com/' style={{}}> <Image style={style.subImg} source={require('../img/twitt.svg')}/></a>
+           </View>
+            </View>
         </View>
         </SafeAreaView>
     )
@@ -77,5 +85,9 @@ const style = StyleSheet.create({
         backgroundColor:'#575445',
         color:'#575445',
         width:'50%'
+    },
+    subImg:{
+        width:30,
+        height:30
     }
 })
